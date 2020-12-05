1ff455ece6cb6013d266d7f32366d281a8c2dc1f
Ian Hoffman <ianhoffman10@gmail.com>
2020-12-04 22:41:16 -0800

    day5

---
+++ day5/src/main.rs
@@ -0,0 +1,52 @@
+use std::fs;
+
+fn binary_partition(line: &str, target: char, mut min_val: u32, mut max_val: u32) -> u32 {
+    for c in line.chars() {
+        if c == target {
+            max_val = (min_val + max_val) / 2;
+        } else {
+            min_val = (min_val + max_val) / 2 + 1;
+        }
+    }
+    min_val
+}
+
+fn get_row_num(line: &str) -> u32 {
+    binary_partition(line, 'F', 0, 127)
+}
+
+fn get_seat_num(line: &str) -> u32 {
+    binary_partition(line, 'L', 0, 7)
+}
+
+fn get_seat_id(line: &str) -> u32 {
+    return get_row_num(&line[..7]) * 8 + get_seat_num(&line[7..]);
+}
+
+fn get_my_seat_id(seat_ids: Vec<u32>) -> u32 {
+    let mut last_seat_id = 0;
+    let mut my_seat_id = 0;
+    for seat_id in seat_ids.iter() {
+        if seat_id - 2 == last_seat_id {
+            my_seat_id = seat_id - 1;
+            break;
+        }
+        last_seat_id = *seat_id;
+    }
+    my_seat_id
+}
+
+fn main() {
+    let mut seat_ids = fs::read_to_string("input.txt")
+        .expect("Failed to read input")
+        .lines()
+        .map(|line| get_seat_id(&line))
+        .collect::<Vec<u32>>();
+
+    let max_seat_id = seat_ids.iter().max().unwrap();
+    println!("Max seat id: {}", max_seat_id);
+
+    seat_ids.sort();
+    let my_seat_id = get_my_seat_id(seat_ids);
+    println!("My seat id is: {}", my_seat_id);
+}
